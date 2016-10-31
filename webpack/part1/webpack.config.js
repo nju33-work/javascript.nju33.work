3f23b199ef87ecd3d2b85cde1c6e7af818c288c2
nju33 <jun.sasaki@mvrck.co.jp>
2016-10-31 17:09:04 +0900

    Add webpack/part1

--- webpack/part1/webpack.config.js
+++ webpack/part1/webpack.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   devtool: 'cheap-module-source-map',
 
-  resolve: {
-    extensions: ['.js', '.json', '.babel.js'],
-  },
-
   entry: './src/index.js',
   output: {
     filename: './local/scripts/index.js'
